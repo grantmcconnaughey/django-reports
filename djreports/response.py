9213cc5f1ddd856bdb9ab1a8bef0b1532999bebb
Grant McConnaughey <grantmcconnaughey@gmail.com>
2016-01-15 11:11:49 -0600

    Add CSV Response

---
+++ djreports/response.py
@@ -0,0 +1,16 @@
+import csv
+
+from django.http.response import HttpResponse
+
+
+class CSVReportResponse(HttpResponse):
+
+    def __init__(self, report, file_name='report.csv', *args, **kwargs):
+        kwargs['content_type'] = 'text/csv'
+        super(CSVReportResponse, self).__init__(*args, **kwargs)
+        self['Content-Disposition'] = 'attachment; filename="{}}"'.format(file_name)
+
+         writer = csv.writer(self)
+         writer.writerow(report.headers)
+         for row in report.data:
+             writer.writerow(row)
