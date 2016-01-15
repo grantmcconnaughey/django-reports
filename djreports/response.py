e146cd8f33587aa07db836f194514a22c38e645b
Grant McConnaughey <grantmcconnaughey@gmail.com>
2016-01-15 11:14:20 -0600

    Fix wonky indentation

--- djreports/response.py
+++ djreports/response.py
@@ -8,9 +8,9 @@ class CSVReportResponse(HttpResponse):
     def __init__(self, report, file_name='report.csv', *args, **kwargs):
         kwargs['content_type'] = 'text/csv'
         super(CSVReportResponse, self).__init__(*args, **kwargs)
-        self['Content-Disposition'] = 'attachment; filename="{}}"'.format(file_name)
+        self['Content-Disposition'] = 'attachment; filename="{}"'.format(file_name)
 
-         writer = csv.writer(self)
-         writer.writerow(report.headers)
-         for row in report.data:
-             writer.writerow(row)
+        writer = csv.writer(self)
+        writer.writerow(report.headers)
+        for row in report.data:
+            writer.writerow(row)
